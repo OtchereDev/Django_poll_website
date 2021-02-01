12a58364c8daed7c13c114dd83c7274f0ff3907f
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-01 22:23:40 +0000

    finished initial configuration

--- Django_poll_website/settings.py
+++ Django_poll_website/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+
+STATIC_ROOT=BASE_DIR/'static_root'
+MEDIA_ROOT=BASE_DIR/'media_root'
+
+STATICFILES_DIR=[
+    BASE_DIR/'static',
+]
+
