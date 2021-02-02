8bce934f434d4187263dc05dd3e13ebe5e29a5bb
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-02 21:02:44 +0000

    added poll system models

--- poll_question/models.py
+++ poll_question/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    questions=models.ManyToManyField('Question')
+    created_by=models.CharField(max_length=250)
+    date_created=models.DateTimeField(auto_now_add=True)
+    signin_vote_only=models.BooleanField(default=False)
+
+
+class Question(models.Model):
+    question=models.TextField()
+    answers=models.ManyToManyField('Answer')
+
+
+class Answer(models.Model):
+    content=models.CharField(max_length=550)
+    count=models.IntegerField(default=0)
