a3fa87e08c8599de8425601865e7d94ec11bf317
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-02 21:01:37 +0000

    added dummy view & url to test form logic

--- poll_question/views.py
+++ poll_question/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .form import TestForm
 
 # Create your views here.
+
+def home(request):
+    form=TestForm()
+    return render(request,'index.html',context={
+        'form':form
+    })
+
+
+# poll templates needed :
+# homepage
+# sign up
+# sign in
+# sign out 
+# create a poll
+# take a poll 
+# poll results**
