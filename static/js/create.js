458724ed847233fd7002cfe715d70fd9f4db8b89
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-07 21:37:17 +0000

    added plus icon to question btn

--- static/js/create.js
+++ static/js/create.js
@@ -10,8 +10,10 @@ const title_div=`
         <h2 class='text-muted text-centered  create_title'>
         Create Your Poll below
            </h2>
-        <button class="btn btn-success ques_btn d-inline">
-           Add a question
+        <button class="btn btn-success ques_btn d-inline-flex align-items-center">
+            <svg class="mx-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>Add a question
         </button>
     </div>
 `
@@ -53,7 +55,7 @@ function answer_creator(answer_num){
 answer.classList.add('form-control')
 answer.classList.add('my-2')
 answer.classList.add('answer')
-answer.classList.add(answer_num)
+answer.name=answer_num
 answer.placeholder="Enter the answer"
 answer.type='text'
 
@@ -64,7 +66,7 @@ return answer
 
 const form_submit_sec=`
 <div class='text-centered'>
-<button type="button" class="submit_btn btn btn-success">Register Poll</button>
+<button type="submit" class="submit_btn btn btn-success">Register Poll</button>
 </div>
 `
 
@@ -96,21 +98,25 @@ create_btn.addEventListener('click',e=>{
 
         if (submit_btn===null){
             
-            let current_ques_num= poll_form.querySelectorAll('div').length+1
+            let current_ques_num= poll_form.querySelectorAll('.question_answer').length+1
             poll_form.insertBefore(question_ceator('question_'+current_ques_num),null)
             const questionEl=poll_form.querySelector('.question_answer')
             poll_form.innerHTML += form_submit_sec
             
         }else{
-            let current_ques_num= poll_form.childElementCount-1
+            let current_ques_num= poll_form.childElementCount-3
             poll_form.insertBefore(question_ceator('question_'+current_ques_num),submit_btn.parentNode)
         }
         
 
         poll_form.addEventListener('click',e=>{
+
+            
             if (e.target.classList.contains('ans_btn')){
                 
                 const parent=e.target.parentNode
+
+                console.log(parent.classList[0])
 
                 let ques_num = parseInt(parent.classList[0].split('_')[1])
 
@@ -118,7 +124,7 @@ create_btn.addEventListener('click',e=>{
 
                 const ans_btn=parent.querySelector('.ans_btn')
 
-                parent.insertBefore(answer_creator(answer_num),ans_btn)
+                parent.insertBefore(answer_creator('answer_'+answer_num+'_q'+ques_num),ans_btn)
 
                
                 
@@ -134,6 +140,12 @@ create_btn.addEventListener('click',e=>{
 
                 submit_btn.remove()
             }
+        })
+
+        poll_form.addEventListener('submit',e=>{
+            e.preventDefault()
+            console.log('submited')
+            console.log(Array.from(poll_form.querySelectorAll('input,textarea')).reduce((acc,input)=>({...acc,[input.name]:input.value}),{}))
         })
         
     })
