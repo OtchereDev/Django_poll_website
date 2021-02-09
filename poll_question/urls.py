a8790eec03ac24bb72bca13f5c192b5b69ffc93a
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-09 20:21:41 +0000

    added url for taking poll

--- poll_question/urls.py
+++ poll_question/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,create_form
+from .views import home,create_form,take_poll
 
 app_name='poll_app'
 
 urlpatterns = [
     path('',home,name='home'),
     path('create/',create_form,name='poll_create'),
+    path('take_poll/',take_poll,name='take_poll'),
 ]
