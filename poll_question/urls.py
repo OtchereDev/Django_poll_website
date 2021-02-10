0669b10b83532de891671c453bb2ebe0e0bebfe8
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-10 12:45:30 +0000

    added view to urls and migrated

--- poll_question/urls.py
+++ poll_question/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home,create_form,take_poll,answers
+from .views import home,create_form,take_poll,answers,returnUser
 
 app_name='poll_app'
 
 urlpatterns = [
     path('',home,name='home'),
     path('create/',create_form,name='poll_create'),
-    path('take_poll/',take_poll,name='take_poll'),
+    path('take_poll/<uuid:uuid>',take_poll,name='take_poll'),
     path('answers/',answers,name='answers'),
+    path('users/',returnUser,name='user'),
 ]
