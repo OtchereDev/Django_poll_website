9f19fb9754f0d4a09a89d7277cba6c1b4d7cc27d
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-11 21:11:24 +0000

    added custom login view and signup view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .form import createUserForm
+from .models import CustomUser
+from django.contrib.auth.views import auth_login,LoginView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+def signup(request):
+    form=createUserForm(request.POST or None)
+
+    if form.is_valid():
+        
+        data=form.cleaned_data
+
+        user=CustomUser.objects.create_user(email=data['email'],first_name=data['first_name'],
+                                            password=data['password1'],)
+
+        if user:
+            auth_login(request,user=user)
+            messages.add_message(request, messages.INFO, 'Successfully created an account')
+
+            return redirect('poll_app:home')
+
+        
+        print(user)
+        
+
+    return render(request,'registration/signup.html',{
+        'form':form
+    })
+
+# class LogIn(SuccessMessageMixin,LoginView):
+#     success_message='Successfully signed in.'
+#     success_url='/'
+
+class LogIn(SuccessMessageMixin, LoginView):
+    template_name = 'registration/login.html'
+    success_url = 'poll_app:home'
+    success_message = "You were successfully logged in."
+
+
