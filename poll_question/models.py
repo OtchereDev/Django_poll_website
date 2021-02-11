21f47214ac9251fd0970b654e08e2031bc0dffd1
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-11 21:16:23 +0000

    added voters model and registered it

--- poll_question/models.py
+++ poll_question/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+from authentication.models import CustomUser
+
 import uuid
 
 
 class Poll(models.Model):
-    questions=models.ManyToManyField('Question')
-    created_by=models.CharField(max_length=250)
-    date_created=models.DateTimeField(auto_now_add=True)
-    signin_vote_only=models.BooleanField(default=False)
     poll_title=models.TextField()
     poll_description=models.TextField()
+    questions=models.ManyToManyField('Question')
+    responses=models.ManyToManyField('Voter')
     uuid=models.UUIDField(default=uuid.uuid4, editable=False)
-    responses=models.IntegerField(default=0)
+    signin_vote_only=models.BooleanField(default=False)
+    created_by=models.CharField(max_length=250)
+    date_created=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.poll_title +'--'+str(self.uuid)
@@ -32,4 +34,15 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.content
+    
+
+class Voter(models.Model):
+    user=models.ForeignKey(CustomUser,on_delete=models.SET_NULL,null=True,blank=True)
+    browser_id=models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        if self.user:
+            return str(self.user)
+        else:
+            return self.browser_id
     
