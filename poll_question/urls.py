4eb73beeebd3bd186bf75f9101abc4c150ffc4ac
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-12 21:07:42 +0000

    added a seperate url and view for poll creation form

--- poll_question/urls.py
+++ poll_question/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home,create_form,take_poll,answers,returnUser
+from .views import home,create_form,take_poll,answers,returnUser,poll_creation
 
 app_name='poll_app'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('take_poll/<uuid:uuid>',take_poll,name='take_poll'),
     path('answers/',answers,name='answers'),
     path('users/',returnUser,name='user'),
+    path('create_poll',poll_creation),
 ]
